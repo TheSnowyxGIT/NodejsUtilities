0498882dabc21e6cd8605c797401286278c51bf4
adrien.pingard <adrien.pingard@epita.fr>
2022-03-30 16:13:04 +0000

    adapt index for typescript

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,4 @@
 /*         authors: adrien.pgd      Licence: MIT                */
 /** ============================================================*/
 
-
-console.log("tmp version")
-/*
-module.exports = {
-    Queue: require("./src/queue"),
-    Tasks: require("./src/task")
-};
-*/
+export {Queue} from "./classes/queue";
